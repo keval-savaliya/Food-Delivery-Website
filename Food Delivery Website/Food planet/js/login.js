d3011bb7856f3189b3eac5c07dbb78d3a7bfe544
keval-savaliya <kpsavaliya1@gmail.com>
2024-08-15 02:40:39 +0530

    First

---
+++ Food Delivery Website/Food planet/js/login.js
@@ -0,0 +1,40 @@
+const delay = (delayInms) => {
+    return new Promise(resolve => setTimeout(resolve, delayInms));
+  }
+
+async function validation() {
+
+    let user = document.getElementById('userName').value
+    let pass = document.getElementById('pwd').value
+
+    let correct = localStorage.getItem(user)
+
+    if (correct == null) {
+        swal({
+            title: "Username not found!",
+            text: "Login failed!",
+            icon: "warning",
+        });
+        return false
+    }
+
+    if (correct!=pass) {
+        swal({
+            title: "Incorrect Password!",
+            text: "Login failed!",
+            icon: "warning",
+        });
+        return false
+    }
+
+    swal({
+        title: "Welcome "+ user,
+        text: "Logged Successfully.!",
+        icon: "success",
+    });
+
+    localStorage.setItem('login',user)
+
+    let delayres = await delay(2000);
+    window.open("./home.html")
+}
